09affda94b73057b32e749135a246c366f6c7728
Manato Takahashi <154682191+manato-takahashi@users.noreply.github.com>
2026-04-26 20:22:47 +0900

    feat: AWS EC2セキュリティグループの取得機能を実装

--- main.go
+++ main.go
@@ -1,7 +1,28 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"fmt"
+	"log"
+
+	"github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/service/ec2"
+)
 
 func main() {
-	fmt.Println("hello sg-mapper")
+	cfg, err := config.LoadDefaultConfig(context.TODO())
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	client := ec2.NewFromConfig(cfg)
+
+	output, err := client.DescribeSecurityGroups(context.TODO(), &ec2.DescribeSecurityGroupsInput{})
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for _, sg := range output.SecurityGroups {
+		fmt.Println(*sg.GroupId, *sg.GroupName)
+	}
 }
